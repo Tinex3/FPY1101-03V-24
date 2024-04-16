75ec2acbff1aaba51a8b2b0a72952dcad6a7524c
ProfeHernanSaavedra <hern.saavedra@profesor.duoc.cl>
2024-04-15 22:02:54 -0400

    calculo de 3 notas

---
+++ CalcularNotas.py
@@ -0,0 +1,22 @@
+# calcular promedio de 3 notas
+# nota 1 30%
+# nota 2 30%
+# nota 3 40%
+
+# Promedio equivale al 60 % de la nota final
+# examen equivale al 40% de l anota final
+# calcule nota final de la asignatura
+
+nota1 = float(input("Ingrese nota 1: ")) # decimales
+nota2 = float(input("Ingrese nota 2: ")) # decimales
+nota3 = float(input("Ingrese nota 3: ")) # decimales
+
+promNotas = nota1*0.3+nota2*0.3+nota3*40/100
+
+notaExamen = float(input("Ingrese nota examen: "))
+
+#examen = notaExamen * 0.4
+
+promFinal = promNotas*0.6 + notaExamen*0.4
+
+print("Su promedio final es: ",promFinal)
