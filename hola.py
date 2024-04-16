d8c2315c84ff0cd872163f58ddd334cc93e803f4
ProfeHernanSaavedra <hern.saavedra@profesor.duoc.cl>
2024-04-15 21:16:48 -0400

    agregue suma de dos numeros

--- hola.py
+++ hola.py
@@ -1,3 +1,7 @@
 print("Hola mundo")
 nombre = input("Ingrese su nombre: ")
 print("Su nombre es: ",nombre)
+num1 = int(input("Ingrese un numero "))
+num2 = int(input("Ingrese otro numero "))
+suma = num1 + num2
+print("la suma de ",num1,"+",num2," es ",suma)
