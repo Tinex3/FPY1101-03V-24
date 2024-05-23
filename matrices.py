df6ef8b44581b81e2ae4c10914295bb60eb32b24
profehernansaavedra <hern.saavedra@profesor.duoc.cl>
2024-05-23 19:53:28 -0400

    ejemplo matrices

--- matrices.py
+++ matrices.py
@@ -4,4 +4,21 @@ matriz_Sencilla = [
 
 ]
 
-#hola soy el profe hernan
+for elemento in matriz_Sencilla:
+    print(elemento)
+
+print(matriz_Sencilla)
+
+print(matriz_Sencilla[0][2])
+
+for fila in matriz_Sencilla:
+    for elemento in fila:
+        print(elemento,end=" ")
+print()
+for i in range(2):
+    for j in range(3):
+        print(matriz_Sencilla[i][j])
+print()
+for fila in range(2):
+    for columna in range(3):
+        print(matriz_Sencilla[fila][columna],end=" ")
