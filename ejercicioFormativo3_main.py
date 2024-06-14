dde364f0d4d58d6d7f581127aca9f622cc7ea740
Hernan Saavedra <hernan.saavedra@gmail.com>
2024-06-13 20:23:55 -0400

    ejercicio formativo 3

---
+++ ejercicioFormativo3_main.py
@@ -0,0 +1,24 @@
+import ejercicioFormativo3_funciones as fn
+
+trabajadores=[]
+
+while True:
+    print("Bienvenidos al sistema de sueldo")
+    print("1. Registrar Trabajadores")
+    print("2. Listar Trabajadores")
+    print("3. Imprimir planilla de sueldos")
+    print("4. Salir")
+    opcion = int(input("Seleccion su opción: "))
+
+    if opcion == 1 :
+        fn.registrar_trabajador(trabajadores)
+    elif opcion == 2:
+        fn.listar_trabajadores(trabajadores)
+    elif opcion == 3:
+        fn.imprimir_planilla(trabajadores)
+        print("¡¡¡¡¡¡¡PLANILLA GENERADA!!!!!!")
+    elif opcion == 4:
+        break
+    else:
+        print("Opción no válida, intente nuevamente")
+
